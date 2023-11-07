695d069ef02b7712e8c618daec038fd1acce19e8
adevjoe <adevjoe@gmail.com>
2023-11-07 12:54:46 +0800

    feat: add support for --disable-openapi-validation flag in uninstall
    Signed-off-by: adevjoe <adevjoe@gmail.com>

--- pkg/action/uninstall.go
+++ pkg/action/uninstall.go
@@ -36,13 +36,15 @@ import (
 type Uninstall struct {
 	cfg *Configuration
 
-	DisableHooks        bool
-	DryRun              bool
-	KeepHistory         bool
-	Wait                bool
-	DeletionPropagation string
-	Timeout             time.Duration
-	Description         string
+	DisableHooks             bool
+	DryRun                   bool
+	IgnoreNotFound           bool
+	KeepHistory              bool
+	Wait                     bool
+	DeletionPropagation      string
+	DisableOpenAPIValidation bool
+	Timeout                  time.Duration
+	Description              string
 }
 
 // NewUninstall creates a new Uninstall object with the given configuration.
@@ -101,7 +103,7 @@ func (u *Uninstall) Run(name string) (*release.UninstallReleaseResponse, error) 
 	res := &release.UninstallReleaseResponse{Release: rel}
 
 	if !u.DisableHooks {
-		if err := u.cfg.execHook(rel, release.HookPreDelete, u.Timeout); err != nil {
+		if err := u.cfg.execHook(rel, release.HookPreDelete, u.Timeout, !u.DisableOpenAPIValidation); err != nil {
 			return res, err
 		}
 	} else {
@@ -134,7 +136,7 @@ func (u *Uninstall) Run(name string) (*release.UninstallReleaseResponse, error) 
 	}
 
 	if !u.DisableHooks {
-		if err := u.cfg.execHook(rel, release.HookPostDelete, u.Timeout); err != nil {
+		if err := u.cfg.execHook(rel, release.HookPostDelete, u.Timeout, !u.DisableOpenAPIValidation); err != nil {
 			errs = append(errs, err)
 		}
 	}
@@ -217,7 +219,7 @@ func (u *Uninstall) deleteRelease(rel *release.Release) (kube.ResourceList, stri
 		builder.WriteString("\n---\n" + file.Content)
 	}
 
-	resources, err := u.cfg.KubeClient.Build(strings.NewReader(builder.String()), false)
+	resources, err := u.cfg.KubeClient.Build(strings.NewReader(builder.String()), !u.DisableOpenAPIValidation)
 	if err != nil {
 		return nil, "", []error{errors.Wrap(err, "unable to build kubernetes objects for delete")}
 	}
